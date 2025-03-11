b13de1a71306b660e81e5caf01da037cca1cd6bd
Vickysonaiya <vickysonaiya0189@gmail.com>
2025-03-11 18:36:14 +0530

    structure changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
